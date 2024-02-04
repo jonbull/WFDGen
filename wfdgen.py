1599035a0230165f3076157c27d8fa0984f5e4b8
jonbull <jonbull@gmail.com>
2024-02-03 17:51:42 -0700

    80% Refactor based on feedback: Discreet classes for read & validation, scoring, logoutput config.ini added Improved adherence to Python style guide
    ToDo:
    Better config.ini handling.
    Line 251 - Handle time better.
    Discuss reuse of findBand/validateBand, scope variables more better.

--- wfdgen.py
+++ wfdgen.py
@@ -1,164 +1,228 @@
-debug = 1
-theFile = 'af7xj2024.csv'
-
-if debug == 1:
-    theFile = 'debug.csv'
-
-class qso:
+class ValidateQSO:
     def __init__(self):
 
-        self.owncall = 'af7xj'
-        self.owncat = '1o'
-        self.ownsec = 'id'
-        self.year = '2024'
-        self.month = '01'
-        self.isQRP = 1
+        myConfig = {}
+        theCSV = open("config.ini")
+        for line in theCSV:
+            if line[0] != '#':
+                row = line.rstrip().split("=")
+                print(row)
+                myConfig[row[0]] = row[1]
 
+        file = myConfig["file"]
+
+        self.rawlist = []
+
+        for line in open(file):
+            self.rawlist.append(line.rstrip().split(","))
+
+        # List of bands and their frequency range
         self.bands = {'160m': [1800, 2000],
-                 '80m': [3500, 4000],
-                 '40m': [7000, 7300],
-                 '20m': [14000, 14350],
-                 '15m': [21000, 21450],
-                 '10m': [28000, 29700],
-                 '2m': [144, 148],
-                 '70cm': [420, 450]
-                 }
+                      '80m': [3500, 4000],
+                      '40m': [7000, 7300],
+                      '20m': [14000, 14350],
+                      '15m': [21000, 21450],
+                      '10m': [28000, 29700],
+                      '2m': [144, 148],
+                      '70cm': [420, 450]
+                      }
 
-        self.sections = ["CO","IA","KS","MN","MO","ND","NE","SD","CT","EMA","ME","NH","RI","VT","WMA","ENY","NLI","NNJ","NNY","SNJ","WNY","DE","EPA","MDC","WPA","AL","GA","KY","NC","NFL","PR","SC","SFL","TN","VA","VI","WCF","AR","LA","MS","NM","NTX","OK","STX","WTX","EB","LAX","ORG","PAC","SB","SCV","SDG","SF","SJV","SV","AK","AZ","EWA","ID","MT","NV","OR","UT","WWA","WY","MI","OH","WV","IL","IN","WI","AB","BC","GH","MB","NB","NL","NS","ONE","ONN","ONS","PE","QC","SK","TER"]
+        # List of ARRL Sections
+        self.sections = ["CO", "IA", "KS", "MN", "MO", "ND", "NE", "SD", "CT", "EMA", "ME", "NH", "RI", "VT", "WMA",
+                         "ENY", "NLI", "NNJ", "NNY", "SNJ", "WNY", "DE", "EPA", "MDC", "WPA", "AL", "GA", "KY", "NC",
+                         "NFL", "PR", "SC", "SFL", "TN", "VA", "VI", "WCF", "AR", "LA", "MS", "NM", "NTX", "OK", "STX",
+                         "WTX", "EB", "LAX", "ORG", "PAC", "SB", "SCV", "SDG", "SF", "SJV", "SV", "AK", "AZ", "EWA",
+                         "ID", "MT", "NV", "OR", "UT", "WWA", "WY", "MI", "OH", "WV", "IL", "IN", "WI", "AB", "BC",
+                         "GH", "MB", "NB", "NL", "NS", "ONE", "ONN", "ONS", "PE", "QC", "SK", "TER"]
 
-        self.classification = ["I","O","M","H"]
+        # List of Winter Field Day categories
+        self.category = ["I", "O", "M", "H"]
 
-        self.contactTypes = ["PH","CW","DG"]
+        # List of Cabrillo valid contact types/
+        self.contactTypes = ["PH", "CW", "DG"]
 
+        self.errorDict = {}
+        self.validQsoList = []
+
+    # Method to build our error log.
+    # Key is the line number from the CSV
+    # Message will be the reason the line may not be valid
+    def adderrorlog(self, key, message):
+        if key in self.errorDict.keys(): # IF the key is not in the dictionary add the key and message
+            self.errorDict[key].append(message)
+        else: # ELSE just add the message
+            self.errorDict[key] = [message]
+
+    def validatelength(self, totalline): # If we don't have 7 elements in a line, that's an error.
+        if len(totalline) != 7:
+            return True
+        else:
+            return False
+
+    def validateband(self, freq): # Freq provided should fall into one of the bands, otherwise we will
+        for currentband in self.bands.keys():
+            if int(freq) >= self.bands[currentband][0] and int(freq) <= self.bands[currentband][1]:
+               return True
+            else:
+                return False
+
+    def validateclass(self, arrlClass): # Class provided should be in the class list.
+        if self.category.count(arrlClass.upper()) == 0:
+            return True
+        else:
+            return False
+
+    def validatesection(self, section): # Section provided should be in the section list.
+        if self.sections.count(section.upper()) == 0:
+            return True
+        else:
+            return False
+
+    def validatecontact(self, contacttype): # Contact Type should be the list of accepted contact types.
+        if self.contactTypes.count(contacttype.upper()) == 0:
+            return True
+        else:
+            return False
+
+    def validatetime(self, time): # Time needs to be between 2400 and 0
+        if (int(time) > 2400) or (int(time) < 0):
+            return True
+        else:
+            return False
+
+    def validate_date(self, date): # Date should be between 1 and 31
+        if (int(date) > 31) or (int(date) < 1):
+            return True
+        else:
+            return False
+
+    def check_line(self): # For each line, validate the available elements
+        for line in range(len(self.rawlist)):
+            if self.validatelength(self.rawlist[line]):
+                self.adderrorlog(line + 1, "QSO line is not 7 elements long.")
+            else:
+                if self.validateband(self.rawlist[line][0]):
+                    self.adderrorlog(line + 1, "Check frequency, may be out of band.")
+                if self.validateclass(self.rawlist[line][2][-1]):
+                    self.adderrorlog(line + 1, "Check class")
+                if self.validatesection(self.rawlist[line][3]):
+                    self.adderrorlog(line + 1, "Check section")
+                if self.validatetime(self.rawlist[line][4]):
+                    self.adderrorlog(line + 1, "Check time - should be > 0000 and < 2400")
+                if self.validatecontact(self.rawlist[line][5]):
+                    self.adderrorlog(line + 1, "Check mode - should be PH, CW of DG")
+                if self.validate_date(self.rawlist[line][6]):
+                    self.adderrorlog(line + 1, "Check date - should be > 0 and < 31")
+
+        # Go through our QSO list, if there isn't an entry in the errorDict
+        # than write the row to our validQsoList
+        for line in range(len(self.rawlist)):
+            if line + 1 not in self.errorDict.keys():
+                self.validQsoList.append(self.rawlist[line])
+
+        return self.validQsoList
+
+    # Print the errorDict
+    def print_error_log(self):
+        for thiskey in self.errorDict.keys():
+            print("Line " + str(thiskey) + ":")
+            for report in self.errorDict[thiskey]:
+                print(report)
+
+
+class Score:
+    def __init__(self, goodList):
+
+        self.goodList = goodList # Our good QSO list
+
+        # Dictionary of bands and their frequency ranges
+        self.bands = {'160m': [1800, 2000],
+                      '80m': [3500, 4000],
+                      '40m': [7000, 7300],
+                      '20m': [14000, 14350],
+                      '15m': [21000, 21450],
+                      '10m': [28000, 29700],
+                      '2m': [144, 148],
+                      '70cm': [420, 450]
+                      }
+
+        # Dictionary to count our QSOs by type
         self.qsoCount = {'CW': 0,
-                    'PH': 0,
-                    'DG': 0}
+                         'PH': 0,
+                         'DG': 0}
 
+        # Empty set to track our Bands+Modes for multiplier
         self.multiplier = set()
 
-        self.errorBlob = {}
-        self.qsoList = []
+        self.myConfig = {}
+        theCSV = open("config.ini")
+        for line in theCSV:
+            if line[0] != '#':
+                row = line.rstrip().split("=")
+                self.myConfig[row[0]] = row[1]
 
-    def addErrorLog(self,key,message):
-        #IF the key is not in the dictionary add the key and message
-        if key in self.errorBlob.keys():
-            self.errorBlob[key].append(message)
-        else:
-            self.errorBlob[key] = [message]
-
-    """
-    valdiate column counts per row, discard rows that are over under and add them to the log blob
-    """
-    def validateQsoList(self, rawList):
-        for line in range(len(rawList)):
-            if len(rawList[line]) > 7:
-                self.addErrorLog((line + 1),"More than 7 Elements.")
-            elif len(rawList[line]) < 7:
-                self.addErrorLog((line + 1), "Less than 7 Elements.")
-            self.qsoList.append(rawList[line])
-
-    """
-    Find Band is used for determining band multiplier and testing for OOB elements
-    """
     def findBand(self, freq):
-        qsoband = 'OOB'
+        qsoband = "OOB"
         for currentband in self.bands.keys():
             if freq >= self.bands[currentband][0] and freq <= self.bands[currentband][1]:
                 qsoband = currentband
         return qsoband
 
-    """
-    validate freq
-    """
-    def validateFreq(self):
-        for line in range(len(self.qsoList)):
-            if len(self.qsoList[line]) >= 1:
-                if (wfdEntry.findBand(int(self.qsoList[line][0])) == "OOB"):
-                    self.addErrorLog((line + 1), "May be OOB.")
+    def gen_score(self):
+        total_points = 0
 
-    """
-    Validate class
-    """
-    def validateClass(self):
-        for line in range(len(self.qsoList)):
-            if len(self.qsoList[line]) >= 3:
-                if (self.classification.count(self.qsoList[line][2][-1].upper()) == 0):
-                    self.addErrorLog((line + 1), "Bad class")
+        for line in self.goodList:
+            self.qsoCount[line[5].upper()] += 1
+            self.multiplier.add(f'{self.findBand(int(line[0]))}+{line[5]}')
 
-    """
-    validate section
-    """
-    def validateSection(self):
-        for line in range(len(self.qsoList)):
-            if len(self.qsoList[line]) >= 4:
-                if (self.sections.count(self.qsoList[line][3].upper()) == 0):
-                    self.addErrorLog((line + 1), "Bad section.")
-
-    """
-    validate contact type
-    """
-    def validateContact(self):
-        for line in range(len(self.qsoList)):
-            if len(self.qsoList[line]) >= 6:
-                if (self.contactTypes.count(self.qsoList[line][5].upper()) == 0):
-                    self.addErrorLog((line + 1), "Bad contact type.")
-
-    """
-    validate time
-    """
-    def validateTime(self):
-        for line in range(len(self.qsoList)):
-            if len(self.qsoList[line]) >= 5:
-                if ((int(self.qsoList[line][4]) > 2400) or (int(self.qsoList[line][4]) < 0)):
-                    self.addErrorLog((line + 1), "Bad time.")
-
-    """
-    validate date
-    """
-    def validateDate(self):
-        for line in range(len(self.qsoList)):
-            if len(self.qsoList[line]) >= 7:
-                if ((int(self.qsoList[line][6]) > 31) or (int(self.qsoList[line][6]) < 1)):
-                    self.addErrorLog((line + 1), "Bad date.")
-
-    """
-    score log
-    """
-    def genScore(self):
-        for line in range(len(self.qsoList)):
-            if line+1 not in self.errorBlob.keys():
-                self.qsoCount[self.qsoList[line][5].upper()]+=1
-                self.multiplier.add(f'{self.findBand(int(self.qsoList[line][0]))}+{self.qsoList[line][5]}')
-
-        totalPoints = 0
         print("\nMode: Contacts -> Points")
+
         for mode in self.qsoCount.keys():
-            print(mode+": "+str(self.qsoCount[mode]),end='')
-            if mode in ["CW","DG"]:
-                print(" -> "+str(2*self.qsoCount[mode]))
-                totalPoints += 2*self.qsoCount[mode]
+            print(mode + ": " + str(self.qsoCount[mode]), end='')
+            if mode in ["CW", "DG"]:
+                print(" -> " + str(2 * self.qsoCount[mode]))
+                total_points += 2 * self.qsoCount[mode]
             else:
                 print(" -> " + str(self.qsoCount[mode]))
-                totalPoints += self.qsoCount[mode]
+                total_points += self.qsoCount[mode]
+
         temp = 0
         for mode in self.qsoCount.keys():
             temp += self.qsoCount[mode]
-        print ("Numbers of QSO: "+str(temp))
-        print ("QSO Points: "+str(totalPoints)+"\n")
 
-        print("Multipliers = "+str(self.multiplier))
-        print("Multiplier Count = "+str(len(self.multiplier)))
+        print("Numbers of QSO: " + str(temp))
+        print("QSO Points: " + str(total_points) + "\n")
+        print("Multipliers = " + str(self.multiplier))
+        print("Multiplier Count = " + str(len(self.multiplier)))
         print("QSO Points * Multiplier Count = Total")
-        print(str(totalPoints)+" * "+str(len(self.multiplier))+" = "+str(totalPoints*len(self.multiplier))+"\n")
-        totalPoints = totalPoints * len(self.multiplier)
-        if self.isQRP == 1:
+        print(str(total_points) + " * " + str(len(self.multiplier)) + " = " + str(total_points * len(self.multiplier)) + "\n")
+        total_points = total_points * len(self.multiplier)
+        if self.myConfig["isQRP"] == "1":
             print("QRP Bonus * 2")
-            print(str(totalPoints)+" * 2 = "+str(totalPoints*2)+"\n")
-            totalPoints = totalPoints * 2
+            print(str(total_points) + " * 2 = " + str(total_points * 2) + "\n")
+            total_points = total_points * 2
 
         print("##############################")
-        print("Total Points = "+str(totalPoints))
+        print("Total Points = " + str(total_points))
         print("##############################")
+
+
+class LogGen:
+    def __init__(self, goodList):
+        self.goodList = goodList
+
+        myConfig = {}
+        theCSV = open("config.ini")
+        for line in theCSV:
+            if line[0] != '#':
+                row = line.rstrip().split("=")
+                myConfig[row[0]] = row[1]
+
+        self.owncall = myConfig["owncall"]
+        self.owncat = myConfig["owncat"]
+        self.ownsec = myConfig["ownsec"]
+        self.year = myConfig["year"]
+        self.month = myConfig["month"]
 
     def padspace(self, tstring, tlength):
         padding = ''
@@ -168,40 +232,30 @@ class qso:
         else:
             padding = ' '
         return padding
+
+    def findBand(self, freq):
+        qsoband = "OOB"
+        for currentband in self.bands.keys():
+            if freq >= self.bands[currentband][0] and freq <= self.bands[currentband][1]:
+                qsoband = currentband
+        return qsoband
+
     def writerecord(self, freq, mode, totaldate, time, owncall, owncat, ownsec, tcall, tcat, tsec):
         file = open(f'{self.owncall}-{self.year}.log', "a")
         file.write(
             f'QSO: {freq}{self.padspace(freq, 7)}{mode.upper()}{self.padspace(mode, 3)}{totaldate}{self.padspace(totaldate, 11)}{time}{self.padspace(time, 5)}{owncall.upper()}{self.padspace(owncall, 8)}{owncat.upper()}{self.padspace(owncat, 4)}{ownsec.upper()}{self.padspace(ownsec, 4)}{tcall.upper()}{self.padspace(tcall, 8)}{tcat.upper()}{self.padspace(tcat, 4)}{tsec.upper()}\n')
         file.close
+    def build_log(self):
+        for line in self.goodList:
+            self.writerecord(line[0], line[5], self.year+"-"+self.month+"-"+line[6], line[4], self.owncall, self.owncat, self.ownsec, line[1], line[2], line[3])
 
-    def generateLog(self):
-        for line in range(len(self.qsoList)):
-            if line+1 not in self.errorBlob.keys():
-                self.writerecord(self.qsoList[line][0], self.qsoList[line][5], self.year+"-"+self.month+"-"+self.qsoList[line][6], self.qsoList[line][4], self.owncall, self.owncat, self.ownsec, self.qsoList[line][1], self.qsoList[line][2], self.qsoList[line][3])
 
-    def errorLog(self):
-        for thiskey in self.errorBlob.keys():
-            print("Line "+str(thiskey)+":")
-            for report in self.errorBlob[thiskey]:
-                print (report)
+validateMyLog = ValidateQSO()  # Create Object
+validQsoList = validateMyLog.check_line()  # Validate the QSO List
+validateMyLog.print_error_log()  # Print the errors
 
-"""
-read in a csv of QSO data
-"""
-masterList = []
+theScore = Score(validQsoList)  # Create object
+theScore.gen_score()  # Generate score
 
-theCSV = open(theFile)
-for line in theCSV:
-    masterList.append(line.rstrip().split(","))
-
-wfdEntry = qso()
-wfdEntry.validateQsoList(masterList)
-wfdEntry.validateFreq()
-wfdEntry.validateClass()
-wfdEntry.validateSection()
-wfdEntry.validateContact()
-wfdEntry.validateTime()
-wfdEntry.validateDate()
-wfdEntry.genScore()
-wfdEntry.generateLog()
-wfdEntry.errorLog()
+thisThing = LogGen(validQsoList)
+thisThing.build_log()
